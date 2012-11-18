1a9bde5910cf4d386ab63f749cbc5a66bcbc39e8
Eddie Santos <easantos@ualberta.ca>
2012-11-18 13:48:52 -0700

    Added StringUtils and got _one_ test. Yay.

--- src/ca/ualberta/cs/c301f12t01/common/Report.java
+++ src/ca/ualberta/cs/c301f12t01/common/Report.java
@@ -25,6 +25,8 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.UUID;
 
+import ca.ualberta.cs.c301f12t01.common.Sharing;
+
 
 /**
  * Report -- Contains Responses that fulfill Tasks.
