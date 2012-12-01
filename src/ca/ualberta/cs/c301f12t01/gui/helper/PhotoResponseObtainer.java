50a3b3342257db24871e7e107760bff29747af49
padlesky <padlesky@ualberta.ca>
2012-11-30 18:47:57 -0700

    Commit so I can pull and fix merge conflicts.

--- src/ca/ualberta/cs/c301f12t01/gui/helper/PhotoResponseObtainer.java
+++ src/ca/ualberta/cs/c301f12t01/gui/helper/PhotoResponseObtainer.java
@@ -17,23 +17,41 @@
  */
 package ca.ualberta.cs.c301f12t01.gui.helper;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+
 import ca.ualberta.cs.c301f12t01.R;
 import ca.ualberta.cs.c301f12t01.common.Response;
+import ca.ualberta.cs.c301f12t01.common.PhotoResponse;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.CompressFormat;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.Gallery;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 /**
  * 
  * @author Bronte Lee
+ * @author padlesky
  *
  */
 public class PhotoResponseObtainer extends ResponseObtainer {
 
+	private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
+	private String encodedImage;
+	private Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+	private byte[] byteArray;
 	/**
 	 * @param view
 	 */
@@ -44,8 +62,7 @@ public class PhotoResponseObtainer extends ResponseObtainer {
 
 	// TODO implement get photo response
 	public Response getResponse() {
-		return null;
-
+		return new PhotoResponse(encodedImage, CompressFormat.PNG);
 	}
 
 	/* We need to be able to click on a button to take a photo */
@@ -57,12 +74,56 @@ public class PhotoResponseObtainer extends ResponseObtainer {
 
 			public void onClick(View arg0) {
 				
+				//Hope hope = new Hope();
+				startPhoto();
+				encodedImage = getPhoto();
+
 				Log.d("Act-lifecycle", "Clicked Photo Button");
 			}
 		});  
 
 	}
 
+	//private class Hope extends Activity {
+		
+		//create new Intent
+		//Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+		
+//		@Override
+//		public void onCreate(Bundle savedInstanceState) {
+//			super.onCreate(savedInstanceState);
+//			startPhoto(getApplicationContext());
+//		}
+//		//Hope's String
+		//byte[] byteArray;
+		public void startPhoto() {
+			// start the video Capture Intent
+			//intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(new File("/sdcard/mp/")));
+			//((Activity) this.getApplicationContext()).startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+			//((Activity) this.getApplicationContext()).startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+		}
+
+		public void onActivityResult(int requestCode, int resultCode, Intent data) {        
+		    if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+		        if (resultCode == Activity.RESULT_OK) {
+		            Bitmap bmp = (Bitmap) data.getExtras().get("data");
+		            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+		            bmp.compress(Bitmap.CompressFormat.PNG, 100, stream);
+		            byteArray = stream.toByteArray();
+
+		        } else if (resultCode == Activity.RESULT_CANCELED) {
+		            // User cancelled the image capture
+		        } else {
+		            // Image capture failed, advise user
+		        }
+		    }               
+		}
+		
+		public String getPhoto() {
+			String encodeImage = Base64.encodeToString(byteArray, Base64.DEFAULT);
+			return encodeImage;
+		}
+	//}
 	/* (non-Javadoc)
 	 * @see ca.ualberta.cs.c301f12t01.gui.helper.ResponseObtainer#setupFulfillRequest()
 	 */
@@ -79,7 +140,10 @@ public class PhotoResponseObtainer extends ResponseObtainer {
 	@Override
 	public void setupDisplayResponse(Response response) {
 		// TODO Auto-generated method stub
-		
+		ImageView imageView = (ImageView) getView().findViewById(R.id.image_response);
+		byte[] decoded = Base64.decode(encodedImage, Base64.DEFAULT);  
+		Bitmap bitMap = BitmapFactory.decodeByteArray(decoded , 0, decoded.length);
+		imageView.setImageBitmap(bitMap);
 	}
 
 }
